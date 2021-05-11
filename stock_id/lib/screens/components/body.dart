607729cd42d756e79ae0ab7dbacd6d6d9413e3d5
atika1004 <istiqomahatika.ai@gmail.com>
2021-05-12 05:18:55 +0700

    pembuatan header

--- stock_id/lib/screens/components/body.dart
+++ stock_id/lib/screens/components/body.dart
@@ -1,60 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:stock_id/constants.dart';
+import 'package:stock_id/screens/components/header_with_box.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.2,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: stockPrimary,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: stockPadding),
-                  padding: EdgeInsets.symmetric(horizontal: stockPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: stockPrimary.withOpacity(0.30),
-                      )
-                    ],
-                  ),
-                  child: Text(
-                    'Selamat datang di aplikasi stock.id',
-                    style: TextStyle(
-                      color: stockText,
-                      fontSize: 20,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderBox(size: size)
+        ],
+      ),
     );
   }
 }
+
