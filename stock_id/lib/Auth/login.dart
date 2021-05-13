e1ecd38d447d0b9c1c5322c66aedf09b68437699
jeremmyhesa <jeremmyhesa10@gmail.com>
2021-05-13 14:12:31 +0700

    auth

---
+++ stock_id/lib/Auth/login.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(8),
+        color: Color(0xfffff0c8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              decoration:
+                  BoxDecoration(color: Colors.black87, shape: BoxShape.circle),
+              child: Center(
+                child: Icon(
+                  Icons.person,
+                  size: 40,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Selamat Datang, Silahkan Masuk",
+              style: TextStyle(fontSize: 20, color: Colors.black87),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black87)),
+                  prefixIcon: Icon(
+                    Icons.person,
+                    size: 40,
+                  ),
+                  hintText: "Masukkan Username",
+                  hintStyle: TextStyle(color: Colors.black87),
+                  labelText: "Username",
+                  labelStyle: TextStyle(color: Colors.black87)),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black87)),
+                  prefixIcon: Icon(
+                    Icons.person,
+                    size: 40,
+                  ),
+                  hintText: "Masukkan Password",
+                  hintStyle: TextStyle(color: Colors.black87),
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: Colors.black87)),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Card(
+              color: Colors.black87,
+              elevation: 5,
+              child: Container(
+                height: 50,
+                child: InkWell(
+                  splashColor: Colors.white,
+                  onTap: () {},
+                  child: Center(
+                    child: Text(
+                      "Masuk",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text("Belum punya akun ?"),
+            Container(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                child: Center(
+                  child: Text("Daftar disini"),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- stock_id/lib/main.dart
+++ stock_id/lib/main.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:stock_id/constants.dart';
-import 'package:stock_id/screens/home/home_screen.dart';
+import 'package:stock_id/Auth/login.dart';
+// import 'package:stock_id/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Login',
       debugShowCheckedModeBanner: false,
-      title: 'Stock.Id',
-      theme: ThemeData(
-        scaffoldBackgroundColor: stockBackground,
-        primaryColor: stockPrimary,
-        textTheme: Theme.of(context).textTheme.apply(bodyColor: stockText),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomeScreen(),
+      theme: ThemeData(primaryColor: stockPrimary),
+      home: Login(),
     );
   }
 }
