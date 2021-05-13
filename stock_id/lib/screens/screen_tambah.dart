ae89d9d34594942ed7268cc4c22734b6cdf23acd
tommy1804 <48713990+tommy1804@users.noreply.github.com>
2021-05-13 22:30:42 +0700

    Update screen_tambah.dart
    Update screen_tambah.dart

--- stock_id/lib/screens/screen_tambah.dart
+++ stock_id/lib/screens/screen_tambah.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stock_id/constants.dart';
+import 'package:stock_id/screens/screen_barang.dart';
 
 class TambahScreen extends StatelessWidget {
   @override
@@ -12,8 +13,8 @@ class TambahScreen extends StatelessWidget {
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
-          ),
-        ),
+          ), //TextStyle
+        ), //Text
         backgroundColor: stockPrimary,
         elevation: 0,
         leading: IconButton(
@@ -23,8 +24,8 @@ class TambahScreen extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
             },
-        ),
-      ),
+        ), //IconButton
+      ), //AppBar
       body: Column(
         children: <Widget>[
           TextField(
@@ -32,9 +33,9 @@ class TambahScreen extends StatelessWidget {
               labelText: "Nama Barang",
               labelStyle: TextStyle(
               fontSize: 12,
-              )
-            )
-          ),
+              ) //TextStyle
+            ) //InputDecoration
+          ), //TextField
           TextField(
             decoration: InputDecoration(
               suffixIcon: Container(
@@ -43,48 +44,48 @@ class TambahScreen extends StatelessWidget {
                   children: [
                     IconButton(onPressed: () {},
                                icon: Icon(Icons.add_circle_outline),
-                               ),
+                               ), //IconButton
                     IconButton(icon: Icon(Icons.do_disturb_on_outlined), onPressed: () {})
                     ]
-                  ),
-                )
-              )
-            ),
+                  ), //Row
+                ) //Container
+              ) //InputDecoration
+            ), //TextField
           Row(children: [
             Expanded(
               child: Container(
                 padding: EdgeInsets.symmetric(
                   horizontal: 10.0
-                  ),
+                  ), //EdgeInsets.symmetric
                 child:TextField(
                   decoration: InputDecoration(
                     labelText: "Harga Beli",
                     labelStyle: TextStyle(
                       fontSize: 12,
-                      ),
+                      ), //TextStyle
                     suffixIcon: IconButton (
                       onPressed: () {},
                       icon: Icon(Icons.create_outlined),
-                      )
-                    )
-                  ),
-                ),
-              ),
+                      ) //IconButton
+                    ) //InputDecoration
+                  ), //TextField
+                ), //Container
+              ), //Expanded
             Expanded(child: TextField(
               decoration: InputDecoration(
                 labelText: "Harga Jual",
                 labelStyle: TextStyle(
                   fontSize: 12,
-                  ),
+                  ), //TextStyle
                 suffixIcon: IconButton(
                   onPressed: () {},
                   icon: Icon(Icons.create_outlined),
-                  )
-                )
-              )
-            )
+                  ) //IconButton
+                ) //InputDecoration
+              ) //TextField
+            ) //Expanded
         ],
-      ),
+      ), //Row
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -97,12 +98,39 @@ class TambahScreen extends StatelessWidget {
                          Icon(Icons.camera_alt_outlined),
                          Text("Upload Gambar"),
                          ]
-                       )
-                     )
-                ]
-            )
-          ],
-        ),
-    );
+                       ) //Row
+                     ), //TextButton
+          SizedBox(height: 25.0,),
+          Card(
+              color: Colors.black87,
+              elevation: 5,
+              child: Container(
+                height: 50,
+                width: 200,
+                child: InkWell(
+                  splashColor: Colors.white,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BarangScreen()),
+                      );
+                  },
+                  child: Center(
+                    child: Text(
+                      "Tambah Barang",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        ), //TextStyle
+                      ), //Text
+                    ), //Center
+                  ), //InkWell
+                ), //Container
+              ), //Card
+            ]
+          ) //Row
+        ], //<Widget>[]
+      ), //Column
+    ); //Scaffold
   }
 }
