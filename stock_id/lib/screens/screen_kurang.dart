402bc97154b8ff4c877bb1cff0d7697c8d92c66c
tommy1804 <48713990+tommy1804@users.noreply.github.com>
2021-05-13 22:14:15 +0700

    Update screen_kurang.dart
    Update screen_kurang.dart

--- stock_id/lib/screens/screen_kurang.dart
+++ stock_id/lib/screens/screen_kurang.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stock_id/constants.dart';
+import 'package:stock_id/screens/screen_barang.dart';
 
 class KurangScreen extends StatelessWidget {
   @override
@@ -12,8 +13,8 @@ class KurangScreen extends StatelessWidget {
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
-          ),
-        ),
+          ), //TextStyle
+        ), //Text
         backgroundColor: stockPrimary,
         elevation: 0,
         leading: IconButton(
@@ -23,8 +24,8 @@ class KurangScreen extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
             },
-        ),
-      ),
+        ), //IconButton
+       ), //AppBar
       body: Column(
               children: <Widget> [
                 TextField(
@@ -32,36 +33,65 @@ class KurangScreen extends StatelessWidget {
                     labelText: "Nama Barang",
                     labelStyle: TextStyle(
                       fontSize: 12,
-                      ),
+                      ), //TextStyle
                     suffixIcon: IconButton (
                       onPressed: () {},
                       icon: Icon(Icons.create_outlined),
-                      )
-                    )
-                  ),
+                      ) //IconButton
+                    ) //InputDecoration
+                  ), //TextField
                 TextField(
                   decoration: InputDecoration(
                     labelText: "Jumlah Barang",
                     labelStyle: TextStyle(
                       fontSize: 12,
-                      ),
+                      ), //TextStyle
                     suffixIcon: Container(
                       width: 100.0,
                       child: Row(
                         children: [
                           IconButton(onPressed: () {},
                                      icon: Icon(Icons.add_circle_outline),
-                                     ),
+                                     ), //IconButton
                           IconButton(
                             icon:Icon(Icons.do_disturb_on_outlined),
-                          onPressed: () {})
+                          onPressed: () {}) //IconButton
                           ]
-                        ),
-                      )
-                    )
+                        ), //Row
+                      ) //Container
+                    ) //InputDecoration
+                  ), //TextField
+                SizedBox(
+                  height: 30.0,
                   ),
-                ]
-        )
-    );
-  }
+                Card(
+                  color: Colors.black87,
+                  elevation: 5,
+                  child: Container(
+                    height: 50,
+                    width: 250,
+                    child: InkWell(
+                      splashColor: Colors.white,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => BarangScreen()),
+                          );
+                      },
+                      child: Center(
+                        child: Text(
+                          "Ambil Barang",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            ), //TextStyle
+                          ), //Text
+                        ), //Center
+                     ), //InkWell
+                  ), //Container
+               ), //Card
+            ] //<Widget>[]
+         ) //Column
+      ); //Scaffold
+   }
 }
